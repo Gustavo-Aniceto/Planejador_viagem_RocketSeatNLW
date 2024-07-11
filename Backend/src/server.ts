f9a1635f60f9a529dd2549e6879197456e1567d7
Gustavo Henrique Aniceto <108236975+Gustavo-Aniceto@users.noreply.github.com>
2024-07-11 11:35:20 -0300

    add db

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -3,23 +3,6 @@ import { prisma } from "./lib/prisma";
 
 const app = fastify();
 
-app.get('/cadastrar', async (request, reply) => {
-    await prisma.trip.create({
-        data: {
-            destination: 'Florianopolis',
-            starts_at: new Date(),
-            ends_at: new Date(),
-        },
-    });
-    return 'Registro cadastrado com sucesso!';
-});
-
-app.get('/listar', async () => {
-    const trips = await prisma.trip.findMany()
-
-    return trips
-});
-
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running");
 });
